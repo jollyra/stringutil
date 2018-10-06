70a162b20204ed6b7e275240c42eb4cc9db1f509
Nigel Rahkola <me@nigelrahkola.com>
2018-10-06 17:12:04 -0400

    Add string permutations util function

--- sliceutil.go
+++ sliceutil.go
@@ -9,3 +9,26 @@ func IndexOf(strings []string, target string) int {
 	}
 	return -1
 }
+
+// Permutations populates `dest` with all possible permutations of the input
+// slice `xs`.
+func Permutations(dest *[][]string, xs []string, unfixed int) {
+	if len(xs)-1 == unfixed {
+		*dest = append(*dest, xs)
+	}
+
+	for i := unfixed; i < len(xs); i++ {
+		ys := CopySlice(xs)
+		ys[unfixed], ys[i] = ys[i], ys[unfixed]
+		Permutations(dest, ys, unfixed+1)
+	}
+}
+
+// CopySlice returns a copy of the input slice `xs`.
+func CopySlice(xs []string) []string {
+	ys := make([]string, len(xs))
+	for i := range xs {
+		ys[i] = xs[i]
+	}
+	return ys
+}
